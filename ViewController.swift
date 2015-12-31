0e647cf3c4fd673b6d30b2b66e9124ece264ad4f
ChaseDrum <chasedrum@mac.com>
2015-12-31 15:41:19 -0800

    added files for fun

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fun-test
+//
+//  Created by Chase Drum on 12/31/15.
+//  Copyright © 2015 Chase Drum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+// this is a test, making a change to see how it looks in git under terminal
+
+}
+
